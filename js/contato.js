016db0775a9ce8a5ff24ee77c1eb7aa3d76a1d6c
Guilherme Abreu <guilhermesabreu08@gmail.com>
2024-12-18 23:46:59 -0300

    Minificando - Parte 3

--- js/contato.js
+++ js/contato.js
@@ -1,10 +1,1 @@
-let nome = document.getElementById('name')
-let email = document.getElementById('email')
-let mensagem = document.getElementById('mensagem')
-let msg_sucesso = document.querySelector('#mensagem-sucesso')
-
-function Enviar(){
-    if (((nome.value != '') && (email.value != '') && (mensagem.value != ''))) {
-        msg_sucesso.style.display = 'block'
-    }
-}
+let nome=document.getElementById('name');let email=document.getElementById('email');let mensagem=document.getElementById('mensagem');let msg_sucesso=document.querySelector('#mensagem-sucesso');function Enviar(){if(((nome.value!='')&&(email.value!='')&&(mensagem.value!=''))){msg_sucesso.style.display='block'}}
